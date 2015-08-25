501c1da078453d2f2cf7cee2c42d73fecd59d34c
David Muriel <dmuriel@bitergia.com>
2015-08-25 12:57:51 +0200

    Use official node baseimage for iota-lwm2m.

--- images/idas/iota-lwm2m/0.2.0/docker-entrypoint.sh
+++ images/idas/iota-lwm2m/0.2.0/docker-entrypoint.sh
@@ -1,33 +1,6 @@
 #!/bin/bash
 set -e
 
-[ -z "${MONGODB_HOSTNAME}" ] && echo "MONGODB_HOSTNAME is undefined.  Using default value of 'mongodb'" && export MONGODB_HOSTNAME=mongodb
-[ -z "${MONGODB_PORT}" ] && echo "MONGODB_PORT is undefined.  Using default value of '27017'" && export MONGODB_PORT=27017
-[ -z "${MONGODB_DATABASE}" ] && echo "MONGODB_DATABASE is undefined.  Using default value of 'iot-lwm2m'" && export MONGODB_DATABASE=iot-lwm2m
-[ -z "${ORION_HOSTNAME}" ] && echo "ORION_HOSTNAME is undefined.  Using default value of 'orion'" && export ORION_HOSTNAME=orion
-[ -z "${ORION_PORT}" ] && echo "ORION_PORT is undefined.  Using default value of '10026'" && export ORION_PORT=10026
-[ -z "${IOTA_SERVER_PORT}" ] && echo "IOTA_SERVER_PORT is undefined.  Using default value of '4041'" && export IOTA_SERVER_PORT=4041
-[ -z "${IOTA_DEFAULT_SERVICE}" ] && echo "IOTA_DEFAULT_SERVICE is undefined.  Using default value of 'bitergiaidas'" && export IOTA_DEFAULT_SERVICE=bitergiaidas
-[ -z "${IOTA_DEFAULT_SUBSERVICE}" ] && echo "IOTA_DEFAULT_SUBSERVICE is undefined.  Using default value of '/devices'" && export IOTA_DEFAULT_SUBSERVICE=/devices
-[ -z "${IOTA_PATH}" ] && echo "IOTA_PATH is undefined.  Using default value of '/opt/lightweightm2m-iotagent'" && export IOTA_PATH=/opt/lightweightm2m-iotagent
-[ -z "${DEFAULT_MAX_TRIES}" ] && echo "DEFAULT_MAX_TRIES is undefined.  Using default value of '60'" && export DEFAULT_MAX_TRIES=60
-
-if [ -z "${IOTA_SERVER_IP}" ]; then
-    echo "IOTA_SERVER_IP is undefined.  Using container IP".
-    # get container IP for providerUrl
-    IOTA_SERVER_IP=$( grep ${HOSTNAME} /etc/hosts | awk '{print $1}' )
-    [ -z "${IOTA_SERVER_IP}" ] && echo "Failed to get container IP for providerUrl." && exit 1
-fi
-
-# fix variables when using docker-compose
-if [[ ${MONGODB_PORT} =~ ^tcp://[^:]+:(.*)$ ]] ; then
-    export MONGODB_PORT=${BASH_REMATCH[1]}
-fi
-
-if [[ ${ORION_PORT} =~ ^tcp://[^:]+:(.*)$ ]] ; then
-    export ORION_PORT=${BASH_REMATCH[1]}
-fi
-
 function check_host_port () {
 
     local _timeout=10
@@ -35,9 +8,9 @@ function check_host_port () {
     local _is_open=0
 
     if [ $# -lt 2 ] ; then
-	echo "check_host_port: missing parameters."
-	echo "Usage: check_host_port <host> <port> [max-tries]"
-	exit 1
+        echo "check_host_port: missing parameters."
+        echo "Usage: check_host_port <host> <port> [max-tries]"
+        exit 1
     fi
 
     local _host=$1
@@ -46,34 +19,34 @@ function check_host_port () {
     local NC=$( which nc )
 
     if [ ! -e "${NC}" ] ; then
-	echo "Unable to find 'nc' command."
-	exit 1
+        echo "Unable to find 'nc' command."
+        exit 1
     fi
 
     echo "Testing if port '${_port}' is open at host '${_host}'."
 
     while [ ${_tries} -lt ${_max_tries} -a ${_is_open} -eq 0 ] ; do
-	echo -n "Checking connection to '${_host}:${_port}' [try $(( ${_tries} + 1 ))/${_max_tries}] ... "
-	if ${NC} -z -w ${_timeout} ${_host} ${_port} ; then
-	    echo "OK."
-	    _is_open=1
-	else
-	    sleep 1
-	    _tries=$(( ${_tries} + 1 ))
+        echo -n "Checking connection to '${_host}:${_port}' [try $(( ${_tries} + 1 ))/${_max_tries}] ... "
+        if ${NC} -z -w ${_timeout} ${_host} ${_port} ; then
+            echo "OK."
+            _is_open=1
+        else
+            sleep 1
+            _tries=$(( ${_tries} + 1 ))
             if [ ${_tries} -lt ${_max_tries} ] ; then
-		echo "Retrying."
-	    else
-		echo "Failed."
-	    fi
-	fi
+                echo "Retrying."
+            else
+                echo "Failed."
+            fi
+        fi
     done
 
     if [ ${_is_open} -eq 0 ] ; then
-	echo "Failed to connect to port '${_port}' on host '${_host}' after ${_tries} tries."
-	echo "Port is closed or host is unreachable."
-	exit 1
+        echo "Failed to connect to port '${_port}' on host '${_host}' after ${_tries} tries."
+        echo "Port is closed or host is unreachable."
+        exit 1
     else
-	echo "Port '${_port}' at host '${_host}' is open."
+        echo "Port '${_port}' at host '${_host}' is open."
     fi
 }
 
@@ -84,9 +57,9 @@ function check_url () {
     local _ok=0
 
     if [ $# -lt 2 ] ; then
-	echo "check_url: missing parameters."
-	echo "Usage: check_url <url> <regex> [max-tries]"
-	exit 1
+        echo "check_url: missing parameters."
+        echo "Usage: check_url <url> <regex> [max-tries]"
+        exit 1
     fi
 
     local _url=$1
@@ -94,52 +67,86 @@ function check_url () {
     local _max_tries=${3:-${DEFAULT_MAX_TRIES}}
     local CURL=$( which curl )
 
-    if [ ! -e ${CURL} ] ; then
-	echo "Unable to find 'curl' command."
-	exit 1
+    if [[ -z "${CURL}" || ! -e ${CURL} ]] ; then
+        echo "Unable to find 'curl' command."
+        exit 1
     fi
 
     while [ ${_tries} -lt ${_max_tries} -a ${_ok} -eq 0 ] ; do
-	echo -n "Checking url '${_url}' [try $(( ${_tries} + 1 ))/${_max_tries}] ... "
-	if ${CURL} -s ${_url} | grep -q "${_regex}" ; then
-	    echo "OK."
-	    _ok=1
-	else
-	    sleep 1
-	    _tries=$(( ${_tries} + 1 ))
+        echo -n "Checking url '${_url}' [try $(( ${_tries} + 1 ))/${_max_tries}] ... "
+        if ${CURL} -s ${_url} | grep -q "${_regex}" ; then
+            echo "OK."
+            _ok=1
+        else
+            sleep 1
+            _tries=$(( ${_tries} + 1 ))
             if [ ${_tries} -lt ${_max_tries} ] ; then
-		echo "Retrying."
-	    else
-		echo "Failed."
-	    fi
-	fi
+                echo "Retrying."
+            else
+                echo "Failed."
+            fi
+        fi
     done
 
     if [ ${_ok} -eq 0 ] ; then
-	echo "Url check failed after ${_tries} tries."
-	exit 1
+        echo "Url check failed after ${_tries} tries."
+        exit 1
     else
-	echo "Url check succeeded."
+        echo "Url check succeeded."
     fi
 }
 
-check_host_port ${MONGODB_HOSTNAME} ${MONGODB_PORT}
-check_host_port ${ORION_HOSTNAME} ${ORION_PORT}
+if [ $# -eq 0 -o "${1:0:1}" = '-' ] ; then
 
-echo "Testing if orion is ready at http://${ORION_HOSTNAME}:${ORION_PORT}/version"
+    [ -z "${MONGODB_HOSTNAME}" ] && echo "MONGODB_HOSTNAME is undefined.  Using default value of 'mongodb'" && export MONGODB_HOSTNAME=mongodb
+    [ -z "${MONGODB_PORT}" ] && echo "MONGODB_PORT is undefined.  Using default value of '27017'" && export MONGODB_PORT=27017
+    [ -z "${MONGODB_DATABASE}" ] && echo "MONGODB_DATABASE is undefined.  Using default value of 'iot-lwm2m'" && export MONGODB_DATABASE=iot-lwm2m
+    [ -z "${ORION_HOSTNAME}" ] && echo "ORION_HOSTNAME is undefined.  Using default value of 'orion'" && export ORION_HOSTNAME=orion
+    [ -z "${ORION_PORT}" ] && echo "ORION_PORT is undefined.  Using default value of '10026'" && export ORION_PORT=10026
+    [ -z "${IOTA_SERVER_PORT}" ] && echo "IOTA_SERVER_PORT is undefined.  Using default value of '4041'" && export IOTA_SERVER_PORT=4041
+    [ -z "${IOTA_DEFAULT_SERVICE}" ] && echo "IOTA_DEFAULT_SERVICE is undefined.  Using default value of 'bitergiaidas'" && export IOTA_DEFAULT_SERVICE=bitergiaidas
+    [ -z "${IOTA_DEFAULT_SUBSERVICE}" ] && echo "IOTA_DEFAULT_SUBSERVICE is undefined.  Using default value of '/devices'" && export IOTA_DEFAULT_SUBSERVICE=/devices
+    [ -z "${IOTA_PATH}" ] && echo "IOTA_PATH is undefined.  Using default value of '/opt/iotagent-lwm2m'" && export IOTA_PATH=/opt/iotagent-lwm2m
+    [ -z "${DEFAULT_MAX_TRIES}" ] && echo "DEFAULT_MAX_TRIES is undefined.  Using default value of '60'" && export DEFAULT_MAX_TRIES=60
 
-check_url http://${ORION_HOSTNAME}:${ORION_PORT}/version "<version>.*</version>"
+    if [ -z "${IOTA_SERVER_IP}" ]; then
+        echo "IOTA_SERVER_IP is undefined.  Using container IP".
+        # get container IP for providerUrl
+        IOTA_SERVER_IP=$( grep ${HOSTNAME} /etc/hosts | awk '{print $1}' )
+        [ -z "${IOTA_SERVER_IP}" ] && echo "Failed to get container IP for providerUrl." && exit 1
+    fi
 
-# configure iotagent
-sed -i ${IOTA_PATH}/config.js \
-    -e "s|'MONGODB_HOSTNAME'|'${MONGODB_HOSTNAME}'|g" \
-    -e "s|'MONGODB_PORT'|'${MONGODB_PORT}'|g" \
-    -e "s|'MONGODB_DATABASE'|'${MONGODB_DATABASE}'|g" \
-    -e "s|'ORION_HOSTNAME'|'${ORION_HOSTNAME}'|g" \
-    -e "s|'ORION_PORT'|'${ORION_PORT}'|g" \
-    -e "s|port: IOTA_SERVER_PORT|port: ${IOTA_SERVER_PORT}|g" \
-    -e "s|'IOTA_DEFAULT_SERVICE'|'${IOTA_DEFAULT_SERVICE}'|g" \
-    -e "s|'IOTA_DEFAULT_SUBSERVICE'|'${IOTA_DEFAULT_SUBSERVICE}'|g" \
-    -e "s|providerUrl:.*|providerUrl: 'http://${IOTA_SERVER_IP}:${IOTA_SERVER_PORT}',|g"
+    # fix variables when using docker-compose
+    if [[ ${MONGODB_PORT} =~ ^tcp://[^:]+:(.*)$ ]] ; then
+        export MONGODB_PORT=${BASH_REMATCH[1]}
+    fi
 
-exec /sbin/init
+    if [[ ${ORION_PORT} =~ ^tcp://[^:]+:(.*)$ ]] ; then
+        export ORION_PORT=${BASH_REMATCH[1]}
+    fi
+
+    check_host_port ${MONGODB_HOSTNAME} ${MONGODB_PORT}
+    check_host_port ${ORION_HOSTNAME} ${ORION_PORT}
+
+    echo "Testing if orion is ready at http://${ORION_HOSTNAME}:${ORION_PORT}/version"
+
+    check_url http://${ORION_HOSTNAME}:${ORION_PORT}/version "<version>.*</version>"
+
+    # configure iotagent
+    sed -i ${IOTA_PATH}/config.js \
+        -e "s|'MONGODB_HOSTNAME'|'${MONGODB_HOSTNAME}'|g" \
+        -e "s|'MONGODB_PORT'|'${MONGODB_PORT}'|g" \
+        -e "s|'MONGODB_DATABASE'|'${MONGODB_DATABASE}'|g" \
+        -e "s|'ORION_HOSTNAME'|'${ORION_HOSTNAME}'|g" \
+        -e "s|'ORION_PORT'|'${ORION_PORT}'|g" \
+        -e "s|port: IOTA_SERVER_PORT|port: ${IOTA_SERVER_PORT}|g" \
+        -e "s|'IOTA_DEFAULT_SERVICE'|'${IOTA_DEFAULT_SERVICE}'|g" \
+        -e "s|'IOTA_DEFAULT_SUBSERVICE'|'${IOTA_DEFAULT_SUBSERVICE}'|g" \
+        -e "s|providerUrl:.*|providerUrl: 'http://${IOTA_SERVER_IP}:${IOTA_SERVER_PORT}',|g"
+
+    cd ${IOTA_PATH}
+    su - ${IOTA_USER} -c "cd ${IOTA_PATH} ; forever start bin/lwm2mAgent.js"
+    exec su - ${IOTA_USER} -c "forever --fifo logs 0"
+else
+    exec "$@"
+fi
